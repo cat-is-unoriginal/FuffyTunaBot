e6847804a761e85bc7e024aed937f60042cfc07b
unknown <dscoolblade@gmail.com>
2018-07-24 11:12:02 -0500

    added more customizability and other refinements

--- transcend.py
+++ transcend.py
@@ -1,85 +1,100 @@
-# Cats really neato bot: FuffyTuna
-# Version 0.0.5 bby
+# FuffyTuna
+# Version 0.0.6 bby
 # By Cat#5854
 
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import chalk
 import random
 import re
 from random import sample
 
 client = discord.Client()
 sentences = ""
+dadbot_censor = false # people on my server were not happy with the censoring of dadbot ( I love the censorship and begrudgingly obliged )
+tuna_chance = 150 # a one in five hundred chance
 
 @client.event
 async def on_ready():
 	print ("online")
 	print ("name: " + client.user.name)
 	print ("id: " + client.user.id)
-	print ("message of the day: fuck you nibba")
+	print ("ctx of the day: your computer is now infected by ligma")
 	
 @client.event
-async def on_message(message):
+async def on_message(ctx): # I replaced message with ctx since thats what all the cool kids are doing
 
-	luck = random.randint(1,200)
+	luck = random.randint(1,tuna_chance)
 	print("your lucky number is " + str(luck))
 	
-	if message.content.lower() == 'this is so sad alexa play despacito':
+	if ctx.content.lower() == 'this is so sad alexa play despacito' or ctx.content.lower() == 'despacito':
 		twochance = random.randint(1,20)
-		#print("despacito roll: " + str(twochance)
+		print("despacito roll: " + str(twochance)
 		
-		#if twochance == 2:
+		#if twochance == 2: # stupid bug right here about syntax that i can't figure out
 		#	print("Playing Despacito 2")
-		#	await client.send_message(message.channel, 'You have been selected by the Illuminati to listen to Despacito 2.\nPlaying Despacito 2')
-		#	voice = await client.join_voice_channel(message.author.voice_channel)
+		#	await client.send_message(ctx.channel, 'You have been selected by the Illuminati to listen to Despacito 2.\nPlaying Despacito 2')
+		#	voice = await client.join_voice_channel(ctx.author.voice_channel)
 		#	player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=W3GrSMYbkBE')
 		#	player.start()
 			
 		#else:
 		print("Playing Despacito")
-		await client.send_message(message.channel, 'Playing Despacito')
-		voice = await client.join_voice_channel(message.author.voice_channel)
+		await client.send_message(ctx.channel, 'Playing Despacito')
+		voice = await client.join_voice_channel(ctx.author.voice_channel)
+		player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=kJQP7kiw5Fk')
+		player.start()
+		
+	if ctx.content.lower() == 'this is so sad alexa play despayeeto' or ctx.content.lower() == 'despayeeto':
+		twochance = random.randint(1,20)
+		print("Playing Despacito")
+		await client.send_message(ctx.channel, 'Playing Despacito')
+		try:
+			voice = await client.join_voice_channel(ctx.author.voice_channel)
+		except:
+			pass
 		player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=kJQP7kiw5Fk')
 		player.start()
 
-	if message.content.lower() == 'fuffytuna disconnect':
-		await client.send_message(message.channel, 'fine... jeez')
-		voice.disconnect()
+	if ctx.content.lower() == 'fuffytuna disconnect':
+		await client.send_message(ctx.channel, 'fine... jeez')
+		for x in client.voice_clients:	# I don't understand how this words, I just ripped it from stack overflow
+        if(x.server == ctx.message.server):
+            return await x.disconnect()
 		
-	if message.content.lower() == 'fuffytuna please':
-		await client.send_message(message.channel, "Have fun! <https://cdn.discordapp.com/attachments/438354111910379530/465959178775822345/FuffyTuna.txt>")
+	if ctx.content.lower() == 'fuffytuna please':
+		await client.send_message(ctx.channel, "Have fun! <https://cdn.discordapp.com/attachments/438354111910379530/465959178775822345/FuffyTuna.txt>")
 		
-	if message.content.lower() == 'fuffytuna help':
+	if ctx.content.lower() == 'fuffytuna help':
 		print("Helping")
-		await client.send_message(message.channel, """
-		___Heres FuffyTuna's commands! 
+		await client.send_message(ctx.channel, """
+		...Heres FuffyTuna's commands! 
 		FuffyTuna help: 
-		___This 
+		...This 
 		this is so sad alexa play despacito: 
-		___Plays despacito in your voice channel 
+		...Plays despacito in your voice channel 
 		FuffyTuna disconnect: 
-		___Makes FuffyTuna leave the voice channel 
+		... FuffyTuna leave the voice channel 
 		FuffyTuna please: 
-		___Gives you the whole original fanfic 
+		...Gives you the whole original fanfic 
 		""")
+	
+	if dadbot_censor == true:
+		if ctx.content.lower().startswith("im") or ctx.content.lower().startswith("i'm"): #i wonder why .lower() has parenthesis for arguments even though it has none
+			global victim #global bc i needed to use it in the next part the send message part
+			victim = ctx.author.mention
 		
-	if message.content.lower().startswith("im") or message.content.lower().startswith("i'm"):
-		global victim
-		victim = message.author.mention
-		
-	if message.author.id == "247852652019318795" and message.content.startswith("Hi"):
-		await client.delete_message(message)
-		await client.send_message(message.channel, "don\'t you fricking dare dadbot, trying to abuse poor " + victim + ", your on thin ice " + message.author.mention)
-		victim = ""
+		if ctx.author.id == "247852652019318795" and ctx.content.startswith("Hi"):
+			await client.delete_message(ctx)
+			await client.send_message(ctx.channel, "don\'t you fricking dare dadbot, trying to abuse poor " + victim + ", your on thin ice " + ctx.author.mention)
+			victim = ""  https://www.youtube.com/watch?v=jEddfV8Ts3g
 		
 	if luck == 1:
 		print("You win!!")
 		with open('FuffyTunaWithXtraFullstops.txt') as fuffy:
 			sentences = re.findall(r".*?[\.\!\?]+", fuffy.read())
 		bibleverse = sample(sentences, 1)
-		await client.send_message(message.channel, "And I quoth from FuffyTuna.txt \n" + str(bibleverse) + "\n want to read more? Just say \"FuffyTuna please\"")
+		await client.send_message(ctx.channel, "And I quoth from FuffyTuna.txt \n" + bibleverse + "\n want to read more? Just say \"FuffyTuna please\"")
 
-client.run("NDcwNjczNDMxODUxOTU4Mjgy.DjZ_6g.tvio4kQbxLCGFTE2HBPyWCpdxpU")
+client.run("") #Hopefully I don't leak this again
